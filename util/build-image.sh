a140005596e582f6138700214cd85bacea6b8ff0
NormanBenbrahim <norman.benbrahim@gmail.com>
2021-10-22 20:46:08 -0400

    fix

--- util/build-image.sh
+++ util/build-image.sh
@@ -5,3 +5,6 @@ PROJECT_ID=$(gcloud config get-value project)
 gcloud artifacts repositories create point-of-sale-api --repository-format=docker \
 --location=northamerica-northeast2 
 --description="Docker repo for the point of sale api for restaurant menus"
+
+gcloud builds submit \
+--tag northamerica-northeast2-docker.pkg.dev/point-of-sale-329816/point-of-sale-api/point-image:tag1
