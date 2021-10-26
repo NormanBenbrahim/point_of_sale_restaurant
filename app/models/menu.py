78f53246c1c36f6890c4b35917d0f63e3cc08189
NormanBenbrahim <norman.benbrahim@gmail.com>
2021-10-26 19:27:12 -0400

    added column of items as dict using mutable dict utility, so far it isn't breaking the code

--- app/models/menu.py
+++ app/models/menu.py
@@ -1,7 +1,58 @@
 from typing import List
-from flask import current_app
+from flask import current_app, json
+from sqlalchemy.types import TypeDecorator, VARCHAR
+from sqlalchemy.ext.mutable import Mutable
 
 from app.extensions import db
+
+
+# from sqlalchemy docs, helps map dictionaries as json string
+class JSONEncodedDict(TypeDecorator):
+    """
+    Represents an immutable structure as a json-encoded string.
+    """
+
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
+# from sqlalchemy docs, applies mutable mixin to dictionary to allow to update in place
+# as it is serialized
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
+
 
 class MenuModel(db.Model):
     """
@@ -13,14 +64,14 @@ class MenuModel(db.Model):
     name = db.Column(db.String(80), nullable=False, unique=True)
     description = db.Column(db.String(300), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
-    #items = db.Column(db.PickleType, nullable=False)
+    items = db.Column(MutableDict.as_mutable(JSONEncodedDict), nullable=False)
 
     menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
     menu = db.relationship("MenuModel")
 
 
     @classmethod
-    def find_by_name(cls, name: str) -> "MenuModel":
+    def find_by_id(cls, name: str) -> "MenuModel":
         """
         utility to search for menus by name in the database
         """
