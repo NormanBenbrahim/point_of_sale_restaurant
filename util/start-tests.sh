0b6e507bf5510974f9940d44c6f37f42a189af06
NormanBenbrahim <norman.benbrahim@gmail.com>
2021-10-31 14:41:23 -0400

    rename

--- util/start-tests.sh
+++ util/start-tests.sh
@@ -5,9 +5,11 @@ echo "Tests:"
 docker-compose exec api py.test app/tests
 
 ### test coverage
-echo "\nTest coverage:"
+echo ""
+echo "Test coverage:"
 docker-compose exec api py.test --cov-report term-missing app
 
 ### linting
+echo ""
 echo "flake8 linting:"
 #docker-compose exec api flake8 .
